8b81bc4d312a936924c1530eaa2a0bd7a620c873
Md Mitul Hossain <md.mitul.hossen99@gmail.com>
2023-11-07 20:55:06 +0600

    cors origin setup

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ const port = process.env.PORT || 3000;
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+  ],
+  credentials: true,
+}));
 app.use(cookieParser()); 
 
 
